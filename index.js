f84049863efa103db984465f1b39263057c30b38
Sonia <serrano_sonia@hotmail.com>
2021-04-03 20:57:40 -0400

    Done Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Number.parseInt(((Math.random()*10)+1),10);
+
+let num3 = 4;
+let num4 = 8;
+let mod = num3 % num4;
+
+let max = Math.max(1,2,20);
